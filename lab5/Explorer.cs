e1d76ac64f473caf11bd0b9aa6225383ff4c9f76
Elics21 <paw.shweczow@yandex.ru>
2024-03-22 00:50:55 +0700

    lab6 без доп заданий

--- lab5/Explorer.cs
+++ lab5/Explorer.cs
@@ -1,303 +1,1 @@
-using System.IO;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Collections.ObjectModel;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
-using System;
-using System.Reflection;
-using static System.Net.Mime.MediaTypeNames;
-using System.Threading.Tasks;
-using Avalonia.Threading;
-using System.Threading;
-
-namespace lab5_vis
-{
-    public interface IFileSystemExplorer
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public string IconPath { get; set; }
-        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }
-    }
-    public class ParentFolder : IFileSystemExplorer
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public string IconPath { get; set; }
-        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }
-
-        public ParentFolder()
-        {
-            Name = string.Empty;
-            Path = string.Empty;
-            IconPath = "avares://lab5-vis/icons/parent.png";
-        }
-        public ParentFolder(string path, string name)
-        {
-            Name = name;
-            Path = path;
-            IconPath = "avares://lab5-vis/icons/parent.png";
-        }
-    }
-    public class Folder : IFileSystemExplorer
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public string IconPath { get; set; }
-        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }
-
-        public Folder()
-        {
-            Name = string.Empty;
-            Path = string.Empty;
-            IconPath = "avares://lab5-vis/icons/folder.png";
-        }
-        public Folder(string path, string name)
-        {
-            Name = name;
-            Path = path;
-            IconPath = "avares://lab5-vis/icons/folder.png";
-        }
-    }
-    public class File : IFileSystemExplorer
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
-
-        public string IconPath { get; set; } 
-        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }
-
-        public File()
-        {
-            Name = string.Empty;
-            Path = string.Empty;
-            IconPath = "avares://lab5-vis/icons/file.png";
-        }
-        public File(string path, string name)
-        {
-            Name = name;
-            Path = path;
-            IconPath = "avares://lab5-vis/icons/file.png";
-        }
-    }
-    public class Disk : IFileSystemExplorer
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public string IconPath { get; set; }
-        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }
-
-        public Disk()
-        {
-            Name = string.Empty;
-            Path = string.Empty;
-            IconPath = "avares://lab5-vis/icons/driver.png";
-        }
-        public Disk(string path, string name)
-        {
-            Name = name;
-            Path = path;
-            IconPath = "avares://lab5-vis/icons/driver.png";
-        }
-    }
-    public class ImageExplorer : IFileSystemExplorer
-    {
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public string IconPath { get; set; }
-        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }
-        public Bitmap ImageBit { get => new Bitmap(AssetLoader.Open(new Uri(Path))); }
-
-        public ImageExplorer()
-        {
-            Name = string.Empty;
-            Path = string.Empty;
-            IconPath = "avares://lab5-vis/icons/file.png";
-        }
-        public ImageExplorer(string path, string name)
-        {
-            Name = name;
-            Path = path;
-            IconPath = "avares://lab5-vis/icons/file.png";
-        }
-    }
-
-
-    public class  Explorer : INotifyPropertyChanged
-    {
-        public ObservableCollection<IFileSystemExplorer> Items { get { return _items; } set { OnPropertyChanged(); } }
-        ObservableCollection<IFileSystemExplorer> _items = new ObservableCollection<IFileSystemExplorer>();
-        private string _currentDirectory;
-        public string CurrentDirectory
-        {
-            get { return _currentDirectory; }
-            set
-            {
-                if (_currentDirectory != value)
-                {
-                    _currentDirectory = value;
-                    OnPropertyChanged();
-                    LoadItems();
-                }
-            }
-        }
-
-        private Bitmap? _selectedImageBit;
-        public Bitmap? ImageBit
-        {
-            get { return _selectedImageBit; }
-            set
-            {
-                if (_selectedImageBit != value)
-                {
-                    _selectedImageBit = value;
-                    OnPropertyChanged();
-                }
-            }
-        }
-
-        private FileSystemWatcher _fileSystemWatcher;
-        public Explorer()
-        {
-            _currentDirectory = "C:\\test";
-            LoadItems();
-
-            _fileSystemWatcher = new FileSystemWatcher(_currentDirectory);
-            _fileSystemWatcher.IncludeSubdirectories = true; //отслеживаем изменени€ в подкаталогах
-            _fileSystemWatcher.Created += FileSystemWatcher_Changed; //отслеживаем содание
-            _fileSystemWatcher.Deleted += FileSystemWatcher_Changed; //отслеживаем удаление
-            _fileSystemWatcher.Renamed += FileSystemWatcher_Changed; //отслеживаем переиминовку
-            _fileSystemWatcher.EnableRaisingEvents = true; //генераци€ событий
-        }
-
-        private async void LoadItems()
-        {
-            Items.Clear();
-            if(_currentDirectory == "\\")
-            {
-                foreach (var driveInfo in DriveInfo.GetDrives())
-                {
-                    Disk drive = new Disk(driveInfo.Name, driveInfo.Name);
-                    _items.Add(drive);
-                }
-            }
-            else
-            {
-                await LoadDirectoryContentsAsync(_currentDirectory);
-            }
-
-        }
-        private async Task LoadDirectoryContentsAsync(string directory)
-        {
-            string parentDirectory = Path.GetDirectoryName(directory);
-            if (parentDirectory != null)
-            {
-                ParentFolder parentFolder = new ParentFolder(parentDirectory, "...");
-                _items.Add(parentFolder);
-            }
-            else
-            {
-                ParentFolder parentFolder = new ParentFolder("\\", "...");
-                _items.Add(parentFolder);
-            }
-
-            await Task.Delay(1000); // «адержка в 1 секунду
-            var fileSystemInfos = await Task.Run(() => new DirectoryInfo(directory).GetFileSystemInfos());
-
-            foreach (var info in fileSystemInfos)
-            {
-                if (info is FileInfo fileInfo)
-                {
-                    if (IsImage(fileInfo.FullName))
-                    {
-                        ImageExplorer imageExplorer = new ImageExplorer(fileInfo.FullName, fileInfo.Name);
-                        _items.Add(imageExplorer);
-                    }
-                    else
-                    {
-                        File file = new File(fileInfo.FullName, fileInfo.Name);
-                        _items.Add(file);
-                    }
-                }
-                else if (info is DirectoryInfo directoryInfo)
-                {
-                    Folder folder = new Folder(directoryInfo.FullName, directoryInfo.Name);
-                    _items.Add(folder);
-                }
-            }
-        }
-
-        private void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)
-        {
-            //запускаем отдельный поток
-            Task.Run(() =>
-            {
-                //задержка у 0.3с
-                Thread.Sleep(300);
-
-                //перезагружаем элементы
-                Dispatcher.UIThread.InvokeAsync(() =>
-                {
-                    LoadItems();
-                });
-            });
-        }
-
-        public void LoadImage(string path)
-        {
-            if (IsImage(path))
-            {
-                ImageBit = new Bitmap(path);
-            }
-
-        }
-        public void ClickDirectory(string path)
-        {
-            if (Path.GetExtension(path) == string.Empty)
-            {
-                _currentDirectory = path;
-                LoadItems();
-            }
-            else if (IsImage(path))
-            {
-            }
-        }
-
-        public bool IsImage(string path)
-        {
-            string extension = Path.GetExtension(path);
-            if (extension == ".png" || extension == ".jpg")
-            {
-                return true;
-            }
-            else { return false; }
-        }
-
-        //осовобождение ресурсов
-        public void Dispose()
-        {
-            _fileSystemWatcher.Dispose();
-        }
-
-
-
-
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-            field = value;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
-    }
-}
+using System.IO;using System.Collections.Generic;using System.ComponentModel;using System.Runtime.CompilerServices;using System.Collections.ObjectModel;using Avalonia.Media.Imaging;using Avalonia.Platform;using System;using System.Reflection;using static System.Net.Mime.MediaTypeNames;using System.Threading.Tasks;using Avalonia.Threading;using System.Threading;namespace lab5_vis{    public interface IFileSystemExplorer    {        public string Name { get; set; }        public string Path { get; set; }        public string IconPath { get; set; }        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }    }    public class ParentFolder : IFileSystemExplorer    {        public string Name { get; set; }        public string Path { get; set; }        public string IconPath { get; set; }        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }        public ParentFolder()        {            Name = string.Empty;            Path = string.Empty;            IconPath = "avares://lab5-vis/icons/parent.png";        }        public ParentFolder(string path, string name)        {            Name = name;            Path = path;            IconPath = "avares://lab5-vis/icons/parent.png";        }    }    public class Folder : IFileSystemExplorer    {        public string Name { get; set; }        public string Path { get; set; }        public string IconPath { get; set; }        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }        public Folder()        {            Name = string.Empty;            Path = string.Empty;            IconPath = "avares://lab5-vis/icons/folder.png";        }        public Folder(string path, string name)        {            Name = name;            Path = path;            IconPath = "avares://lab5-vis/icons/folder.png";        }    }    public class File : IFileSystemExplorer    {        public string Name { get; set; }        public string Path { get; set; }        public string IconPath { get; set; }         public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }        public File()        {            Name = string.Empty;            Path = string.Empty;            IconPath = "avares://lab5-vis/icons/file.png";        }        public File(string path, string name)        {            Name = name;            Path = path;            IconPath = "avares://lab5-vis/icons/file.png";        }    }    public class Disk : IFileSystemExplorer    {        public string Name { get; set; }        public string Path { get; set; }        public string IconPath { get; set; }        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }        public Disk()        {            Name = string.Empty;            Path = string.Empty;            IconPath = "avares://lab5-vis/icons/driver.png";        }        public Disk(string path, string name)        {            Name = name;            Path = path;            IconPath = "avares://lab5-vis/icons/driver.png";        }    }    public class ImageExplorer : IFileSystemExplorer    {        public string Name { get; set; }        public string Path { get; set; }        public string IconPath { get; set; }        public Bitmap? IconBit { get => new Bitmap(AssetLoader.Open(new Uri(IconPath))); }        public Bitmap ImageBit { get => new Bitmap(AssetLoader.Open(new Uri(Path))); }        public ImageExplorer()        {            Name = string.Empty;            Path = string.Empty;            IconPath = "avares://lab5-vis/icons/file.png";        }        public ImageExplorer(string path, string name)        {            Name = name;            Path = path;            IconPath = "avares://lab5-vis/icons/file.png";        }    }    public class  Explorer : INotifyPropertyChanged    {        public ObservableCollection<IFileSystemExplorer> Items { get { return _items; } set { OnPropertyChanged(); } }        ObservableCollection<IFileSystemExplorer> _items = new ObservableCollection<IFileSystemExplorer>();        private string _currentDirectory;        public string CurrentDirectory        {            get { return _currentDirectory; }            set            {                if (_currentDirectory != value)                {                    _currentDirectory = value;                    OnPropertyChanged();                    LoadItems();                }            }        }        private Bitmap? _selectedImageBit;        public Bitmap? ImageBit        {            get { return _selectedImageBit; }            set            {                if (_selectedImageBit != value)                {                    _selectedImageBit = value;                    OnPropertyChanged();                }            }        }        private FileSystemWatcher _fileSystemWatcher;        public Explorer()        {            _currentDirectory = "C:\\test";            LoadItems();            _fileSystemWatcher = new FileSystemWatcher(_currentDirectory);            _fileSystemWatcher.IncludeSubdirectories = true; //отслеживаем изменени€ в подкаталогах            _fileSystemWatcher.Created += FileSystemWatcher_Changed; //отслеживаем содание            _fileSystemWatcher.Deleted += FileSystemWatcher_Changed; //отслеживаем удаление            _fileSystemWatcher.Renamed += FileSystemWatcher_Changed; //отслеживаем переиминовку            _fileSystemWatcher.EnableRaisingEvents = true; //генераци€ событий        }        private async void LoadItems()        {            Items.Clear();            if(_currentDirectory == "\\")            {                foreach (var driveInfo in DriveInfo.GetDrives())                {                    Disk drive = new Disk(driveInfo.Name, driveInfo.Name);                    _items.Add(drive);                }            }            else            {                await LoadDirectoryContentsAsync(_currentDirectory);            }        }        private async Task LoadDirectoryContentsAsync(string directory)        {            string parentDirectory = Path.GetDirectoryName(directory);            if (parentDirectory != null)            {                ParentFolder parentFolder = new ParentFolder(parentDirectory, "...");                _items.Add(parentFolder);            }            else            {                ParentFolder parentFolder = new ParentFolder("\\", "...");                _items.Add(parentFolder);            }            await Task.Delay(1000); // «адержка в 1 секунду            var fileSystemInfos = await Task.Run(() => new DirectoryInfo(directory).GetFileSystemInfos());            foreach (var info in fileSystemInfos)            {                if (info is FileInfo fileInfo)                {                    if (IsImage(fileInfo.FullName))                    {                        ImageExplorer imageExplorer = new ImageExplorer(fileInfo.FullName, fileInfo.Name);                        _items.Add(imageExplorer);                    }                    else                    {                        File file = new File(fileInfo.FullName, fileInfo.Name);                        _items.Add(file);                    }                }                else if (info is DirectoryInfo directoryInfo)                {                    Folder folder = new Folder(directoryInfo.FullName, directoryInfo.Name);                    _items.Add(folder);                }            }        }        private void FileSystemWatcher_Changed(object sender, FileSystemEventArgs e)        {            //запускаем отдельный поток            Task.Run(() =>            {                //задержка у 0.3с                Thread.Sleep(300);                //перезагружаем элементы                Dispatcher.UIThread.InvokeAsync(() =>                {                    LoadItems();                });            });        }        public void LoadImage(string path)        {            if (IsImage(path))            {                ImageBit = new Bitmap(path);            }        }        public void ClickDirectory(string path)        {            if (Path.GetExtension(path) == string.Empty)            {                _currentDirectory = path;                LoadItems();            }            else if (IsImage(path))            {            }        }        public bool IsImage(string path)        {            string extension = Path.GetExtension(path);            if (extension == ".png" || extension == ".jpg")            {                return true;            }            else { return false; }        }        //осовобождение ресурсов        public void Dispose()        {            _fileSystemWatcher.Dispose();        }        public event PropertyChangedEventHandler? PropertyChanged;        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)        {            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));        }        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)        {            if (EqualityComparer<T>.Default.Equals(field, value)) return false;            field = value;            OnPropertyChanged(propertyName);            return true;        }    }}
---
+++ lab6/Weather.cs
@@ -0,0 +1,194 @@
+﻿using Avalonia.Controls;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace lab6
+{
+    public class WeatherDataService
+    {
+        private readonly WeatherService _weatherService;
+
+        public WeatherDataService()
+        {
+            _weatherService = new WeatherService();
+        }
+
+        public async Task<string> GetWeatherDataAsync(bool isSleep)
+        {
+            return await Task.Run(() => _weatherService.GetDataSync(isSleep));
+        }
+    }
+    public class WeatherData
+    {
+        public string Name { get; set; }
+        public Main main { get; set; }
+        public Wind wind { get; set; }
+        public struct Main
+        {
+            public double Temp { get; set; }
+            public double TempInCelsius
+            {
+                get { return (Temp - 273.17); }
+            }
+            public int Humidity { get; set; }
+            public double pressure { get; set; }
+            public double sea_level {  get; set; }
+        }
+        public struct Wind
+        {
+            public double Speed { get; set; }
+        }
+    }
+
+
+    public class Weather : INotifyPropertyChanged
+    {
+        private readonly WeatherDataService _weatherDataService;
+
+        private string _sity = "...";
+        public string Sity
+        {
+            get { return _sity; }
+            set
+            {
+                _sity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _temperature = "...";
+        public string Temperature
+        {
+            get { return _temperature; }
+            set
+            {
+                _temperature = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _humidity = "...";
+        public string Humidity
+        {
+            get { return _humidity; }
+            set
+            {
+                _humidity = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _wind = "...";
+        public string Wind
+        {
+            get { return _wind; }
+            set
+            {
+                _wind = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _pressure = "...";
+        public string Pressure
+        {
+            get { return _pressure; }
+            set
+            {
+                _pressure = value;
+                OnPropertyChanged();
+            }
+        }
+        private string _seaLevel = "...";
+        public string SeaLevel
+        {
+            get { return _seaLevel; }
+            set
+            {
+                _seaLevel = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Data { get; set; }
+
+        public Weather()
+        {
+            _weatherDataService = new WeatherDataService();
+            MainAsync().ConfigureAwait(false);
+            setData(DateTime.Now);
+
+        }
+
+        private async Task MainAsync()
+        {
+            string weatherData = await _weatherDataService.GetWeatherDataAsync(true);
+            // Обработка полученных данных
+            WeatherData weatherDataObject = JsonConvert.DeserializeObject<WeatherData>(weatherData);
+            Sity = weatherDataObject.Name;
+            double temp = Math.Round(weatherDataObject.main.TempInCelsius);
+            Temperature = temp.ToString() + "°";
+            Humidity = weatherDataObject.main.Humidity.ToString() + "%";
+            Wind = weatherDataObject.wind.Speed.ToString() + " m/s";
+            Pressure = weatherDataObject.main.pressure.ToString() + " mm";
+            SeaLevel = weatherDataObject.main.sea_level.ToString() + " m";
+        }
+
+        private void setData(DateTime date)
+        {
+            string temp = date.ToString();
+            string[] tokens = temp.Split('.');
+            int dayOfWeek = (int)date.DayOfWeek;
+            string dayOfWeekString = "";
+            switch (dayOfWeek)
+            {
+                case 1:
+                    dayOfWeekString = "Понедельник";
+                    break;
+                case 2:
+                    dayOfWeekString = "Вторник";
+                    break;
+                case 3:
+                    dayOfWeekString = "Среда";
+                    break;
+                case 4:
+                    dayOfWeekString = "Четверг";
+                    break;
+                case 5:
+                    dayOfWeekString = "Пятница";
+                    break;
+                case 6:
+                    dayOfWeekString = "Суббота";
+                    break;
+                case 7:
+                    dayOfWeekString = "Воскресенье";
+                    break;
+                default:
+                    break;
+            }
+            Data = dayOfWeekString + ", " + tokens[0];
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
